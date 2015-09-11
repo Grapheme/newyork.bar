81f769af507ed7190f292e09d7e4dde18bab338c
Владимир <vkharseev@gmail.com>
2015-09-11 17:48:08 +0300

    Первый коммит

--- app/modules/application/application.controller.php
+++ app/modules/application/application.controller.php
@@ -4,12 +4,16 @@ class ApplicationController extends BaseController {
 
     public static $name = 'application';
     public static $group = 'application';
+
     /****************************************************************************/
     public static function returnRoutes($prefix = null) {
 
     }
+
     /****************************************************************************/
-	public function __construct(){}
+    public function __construct() {
+    }
+
     /****************************************************************************/
     public static function returnInfo() {
 
